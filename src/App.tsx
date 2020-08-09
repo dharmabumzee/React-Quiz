a5b71ae8ebe170093eee624532642df4d674fc69
lukabelani <luka.belani@gmail.com>
2020-08-09 09:46:43 +0200

    add the question card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { fetchQuizQuestions } from './API';
 
 // Components
-// import QuestionCard from './components/QuestionCard';
+import QuestionCard from './components/QuestionCard';
 
 // Types
 import { QuestionState, Difficulty } from './API';
@@ -52,19 +52,23 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions..</p>
-      {/* <QuestionCard 
-        questionNumber={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
+      {gameOver || userAnswers.length == TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      { !gameOver ? <p className="score">Score:</p> : null }
+      { loading && <p>Loading Questions..</p> }
+      { !loading && !gameOver && (
+          <QuestionCard 
+            questionNumber={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+      )}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
